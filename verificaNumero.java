cfce68b2c9a84d9eb86d587e8dc92b5e18fcf568
kinsk <64823343+deisekinsk@users.noreply.github.com>
2021-05-07 11:00:36 -0300

    Create verificaNumero.java

---
+++ verificaNumero.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Deise Kinsk
+ */
+import java.util.Scanner;
+public class maisVelha {
+    
+       public static void main(String args[]){
+           
+        String nomeA, nomeB;
+        int anoA, anoB;
+        int anoAtual = 2021;
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+
+        nomeA = leitor.nextLine();
+        nomeB = leitor.nextLine();
+
+        System.out.println("Digite o ano:");
+
+        anoA = leitor.nextInt();
+        anoB = leitor.nextInt();
+                
+        if (anoA<anoB){
+          System.out.println(nomeA + " tem " + (anoAtual - anoA) + " anos e é mais velho que " + nomeB + "("+(anoAtual - anoB)+ "anos.)");
+        
+        }else {
+          System.out.println(nomeB + " tem " + (anoAtual - anoB) + " e é mais velho que " + nomeA + "("+(anoAtual - anoA)+ "anos).");
+        }
+
+              
+
+    }
+    
+}
