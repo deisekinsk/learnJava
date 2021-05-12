a87e50517721ad0705f1f174f69429ac533d9984
kinsk <64823343+deisekinsk@users.noreply.github.com>
2021-05-12 00:52:50 -0300

    Create forInfinito.java

---
+++ forInfinito.java
@@ -0,0 +1,17 @@
+package iteracao;
+
+/**
+ *
+ * @author Deise Kinsk
+ */
+public class infinito {
+    
+    public static void main(String[] args) {
+    // TODO code application logic here
+    for (int i = 0; i == i; i++){
+    System.out.println("Impressão nº " + i);
+    }
+        
+  }
+    
+}
