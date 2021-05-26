e941b9328deed7c3ec25d7407b03ce819cb78ed1
kinsk <64823343+deisekinsk@users.noreply.github.com>
2021-05-26 00:32:36 -0300

    Update Carro.java
    Configuração para o objeto DonoDoCarro. Mais um constructor. Metodos para o novo Objeto.

--- POO/Carro.java
+++ POO/Carro.java
@@ -1,13 +1,33 @@
+package estudopoo;
+
+/** EXPLICAÇÃO:
+ * 
+ *
+ * @author Deise Kinsk
+ */
+
 public class Carro {
         //ATRIBUTOS - Os atributos são alterados por métodos de uma classe.
         String corAtributo;
         String tipo;
         String placa;
         int numPortas;
+        int cambio; //testa se o carro ligou.
+        
+        //inserindo o OBJETO DonoDoCarro nos ATRIBUTOS
+        DonoDoCarro dono;
         
          // MAIS DE UM CONSTRUTOR ()
         public Carro(){
             System.out.println("LAVA JATO (ESTUDO DE POO)");
+        }
+        
+        void Funciona(){
+            System.out.println("O carro funciona.");
+        }
+        
+        void Defeito(){
+            System.out.println("O carro está com defeito.");
         }
         
         //CONSTRUCTOR -  representam o código que roda sempre que você usa a palavra-chave new.      
@@ -25,35 +45,54 @@ public class Carro {
         
         //MÉTODO setCor(){}  -  Altera o atributo.
         //PARÂMETRO cor está em laranja. | ATRIBUTO cor está em verde.
-        void setCor(String corParametro){
+        public void setCor(String corParametro){
             //INSTÂNCIAS this.cor
             this.corAtributo = corParametro;
         }
         
         //MÉTODO getCor(){}  -  Retorna o atributo
-        String getCor(){
-            return this.corAtributo;
+        public String getCor(){
+                return this.corAtributo;
         }
         
-        String getTipo(){
-		return tipo;
+        public String getTipo(){
+                return tipo;
 	}
 
-	void setTipo(String tipo){
-		this.tipo = tipo;
-	}
+        public void setTipo(String tipo){
+                this.tipo = tipo;
+        }
 
-	String getPlaca(){
-		return placa;
-	}
-	void setPlaca(String placa){
-		this.placa = placa;
-	}
+        public String getPlaca(){
+                return placa;
+        }
+        public void setPlaca(String placa){
+                this.placa = placa;
+        }
 
-	int getNumPortas(){
-		return numPortas;
-	}
-	void setNumPortas(int numPortas){
-		this.numPortas = numPortas;
-	}  
+        public int getNumPortas(){
+                return numPortas;
+        }
+        public void setNumPortas(int numPortas){
+                this.numPortas = numPortas;
+        } 
+        //Inserindo MÉTODOS para DonoDoCarro
+        public DonoDoCarro getDono(){
+            return dono;
+        }
+        
+        public void setDono(DonoDoCarro dono){
+            this.dono = dono;
+        }
+        
+        public int getCambio(){
+            return this.cambio;
+        }
+        
+        public void setCambio(int cambio){
+            this.cambio = cambio;
+        }
+        
+         
+        
  }
